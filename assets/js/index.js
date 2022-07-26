539b4c1b2d93ecc32f52610a73d41f4c8afce6c9
Jorge Cortes <chochelius@gmail.com>
2022-07-26 08:30:38 -0400

    final

--- assets/js/index.js
+++ assets/js/index.js
@@ -44,137 +44,144 @@ const propiedadesJSON = [
 ];
 
 let rooms = document.getElementById("rooms");
-let minm2 = document.getElementById("minm2");
-let maxm2 = document.getElementById("maxm2");
+let minm2 = document.getElementById("minm2").value;
+let maxm2 = document.getElementById("maxm2").value;
+let filtrado = [];
 let boton = document.getElementById("btn");
 let propiedades = document.querySelector(".propiedades");
 let propiedad = document.querySelectorAll(".propiedad");
+let cantidad = document.getElementById("cantidad");
 
-// clear inputs
-function clearInputs() {
-    rooms.value = "";
-    minm2.value = "";
-    maxm2.value = "";
+const clearInputs = () => {
+    document.getElementById("rooms").value = "";
+    document.getElementById("minm2").value = "";
+    document.getElementById("maxm2").value = "";
 }
 
-let cantidad = document.getElementById("cantidad");
+//validate inputs as numbers
+
+const valido = (e) => {
+    let rooms = document.getElementById("rooms").value;
+    let minm2 = document.getElementById("minm2").value;
+    let maxm2 = document.getElementById("maxm2").value;
+    if (isNaN(rooms) || isNaN(minm2) || isNaN(maxm2)) {
+        alert("los campos deben ser números");
+        init();
+    } else if (rooms < 0 || minm2 < 0 || maxm2 < 0) {
+        alert("los campos deben ser positivos");
+        init();
+    } else {
+        console.log(e)
+        return true;
+    };
+
+}
+
+
+// clean inputs
+
+// filter propiedades
+const filtrar2 = () => {
+    rooms = document.getElementById("rooms").value;
+    minm2 = document.getElementById("minm2").value;
+    maxm2 = document.getElementById("maxm2").value;
+    // clear propiedades
+    propiedades.innerHTML = "";
+    
+    //map array to filter by rooms and m2
+    let filtrado = propiedadesJSON.map(propiedad => {
+        if (propiedad.rooms >= rooms && propiedad.m >= minm2 && propiedad.m <= maxm2) {
+            return propiedad;
+        }
+    }
+    ).filter(propiedad => propiedad !== undefined);
+    console.log(filtrado);
+
+    //render each item of filtrado as a div
+
+    filtrado.forEach(filtrada => {
+        div = document.createElement("div");
+        div.classList.add("propiedad");
+        div.innerHTML = `
+        <img src="${filtrada.src}" alt="" class="img-fluid">                
+        <section>
+            <h5>${filtrada.name}</h5>
+            <div class="d-flex">
+                <p>Cuartos: ${filtrada.rooms}</p>
+                <br>
+                <p>Metros: ${filtrada.m}</p>
+            </div>
+            <p class="my-3">${filtrada.description}</p>
+            <button class="btn btn-info ">Ver más</button onclick(alert = "mas info proximamente")>
+
+        </section>
+    `;
+        propiedades.appendChild(div);
+
+        num = propiedades.childElementCount;
+        cantidad.innerHTML = num;
+
+
+    }   )
+}
+
+    
+
+const filtrar = () => {
+    // clear propiedades
+    propiedades.innerHTML = "";
+    // try to filter2 if valid and show error message if not
+    try {
+        valido();
+        filtrar2();
+    } catch (error) {
+        alert("no hay propiedades que cumplan con los criterios");
+        clearInputs();
+        console.log(error);
+        init();
+    };
+};
+
 
 
 // Función que se ejecuta al cargar la página
 
 function init() {
-    //clear propiedades
+    // clear propiedades
     propiedades.innerHTML = "";
+
+
+
     propiedadesJSON.forEach((propiedad) => {
         let div = document.createElement("div");
         div.classList.add("propiedad");
-        div.innerHTML = `
-                <img src="${propiedad.src}" alt="" class="img-fluid">                
-                <section>
-                    <h5>${propiedad.name}</h5>
-                    <div class="d-flex">
-                        <p>Cuartos: ${propiedad.rooms}</p>
-                        <br>
-                        <p>Metros: ${propiedad.m}</p>
-                    </div>
-                    <p class="my-3">${propiedad.description}</p>
-                    <button class="btn btn-info ">Ver más</button onclick(alert = "mas info proximamente")>
+        div.innerHTML = div.innerHTML = `
+        <img src="${propiedad.src}" alt="" class="img-fluid">                
+        <section>
+            <h5>${propiedad.name}</h5>
+            <div class="d-flex">
+                <p>Cuartos: ${propiedad.rooms}</p>
+                <br>
+                <p>Metros: ${propiedad.m}</p>
+            </div>
+            <p class="my-3">${propiedad.description}</p>
+            <button class="btn btn-info ">Ver más</button onclick(alert = "mas info proximamente")>
 
-                </section>
-            `;
+        </section>
+    `;
         propiedades.appendChild(div);
+        clearInputs();
 
-        num = propiedades.childElementCount;
         // count all propiedades
+        num = propiedades.childElementCount;
         cantidad.innerHTML = num;
 
-        clearInputs();
+        boton = document.getElementById("btn");
+        boton.addEventListener("click", filtrar);
+
+
     });
 }
 
+
 init();
-
-
-
-function filtrar() {
-    // get values from inputs
-    roomsValue = rooms.value;
-    minm2Value = minm2.value;
-    maxvalue = maxm2.value;
-
-    // clear propiedades
-    propiedades.innerHTML = "";
-    //validar inputs
-    valido();
-    // filter propiedades
-    filtrar2();
-    // clear inputs
-    clearInputs();
-    // debug errors
-    console.log(propiedades);
-    
-
-}
-
-//validate inputs as numbers
-
-function valido () {
-    if (isNaN(rooms.value) || isNaN(minm2.value) || isNaN(maxm2.value)){
-        alert("Ingrese un número válido en los campos");
-    }
-    else if (rooms.value == "" || minm2.value == "" || maxm2.value == ""){
-        alert("Ingrese un número válido en los campos");
-    }
-    else if (rooms.value <= 0 || minm2.value <= 0 || maxm2.value <= 0){
-        alert("Ingrese un número válido en los campos");
-    }
-}
-
-// 
-
-// filter propiedades
-function filtrar2() {
-
-    try {
-        propiedadesJSON.forEach((propiedad) => {
-            if (
-                propiedad.rooms >= roomsValue &&
-                propiedad.m >= minm2Value &&
-                propiedad.m <= maxvalue
-            ) {
-                let div = document.createElement("div");
-                div.classList.add("propiedad");
-                div.innerHTML = `
-                <img src="${propiedad.src}" alt="" class="img-fluid">                
-                <section>
-                    <h5>${propiedad.name}</h5>
-                    <div class="d-flex">
-                        <p>Cuartos: ${propiedad.rooms}</p>
-                        <br>
-                        <p>Metros: ${propiedad.m}</p>
-                    </div>
-                    <p class="my-3">${propiedad.description}</p>
-                    <button class="btn btn-info ">Ver más</button onclick(alert = "mas info proximamente")>
-
-                </section>
-            `;
-                propiedades.appendChild(div);
-                num = propiedades.childElementCount;
-                // count all propiedades
-                cantidad.innerHTML = num;
-            }
-            else if ( roomsValue > propiedadesJSON.count) {
-                alert("No hay propiedades con esa cantidad de cuartos");
-            };
-            console.log(propiedades.length);
-        });
-    } catch (error) {
-        console.log(error);
-        init();
-    }
-}
-
-
-
-boton.addEventListener("click", filtrar);
-
